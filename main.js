2bafdefff07af97db1c3f400fd1ccf6d64bb8803
Sabir2408 <casper98187@gmail.com>
2023-01-23 11:57:57 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+//задание №1
+// const userSurname = 'Zaylamov';
+// const userName = 'Lenar';
+// const message = 'Hello';
+// alert (message + userSurname + userName)
+// console.log(userName, userSurname, message); 
+
+//задание №2
+// let number = prompt('ввведите число, сравнив его с числом 10:')
+
+// if(number>10){
+//      alert('Больше');
+// }else if(number=10){
+//      alert('Равно');
+// } else if(number<10){
+//      alert('Меньше');
+// } 
+
+//задание №3
+// let color = prompt("Введите цвет светофора (Красный, Желтый или Зеленый)");
+// switch (color) {
+//   case 'Красный':
+//     alert( 'СТОЙ' );
+//     break;
+
+//   case 'Желтый':
+//     alert( 'Приготовься' );
+//     break;
+
+//   case 'Зеленый':
+//     alert( 'ИДИ!' );
+//     break;
+//   default:
+//     alert( 'Неправильно ввел цвет, попробый первую букву сделать заглавной' );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
